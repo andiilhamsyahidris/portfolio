dbd79f653cfa313493142cb6279c91248d6573e6
iyan <andi.ilhamsyah@digi-id.id>
2024-07-22 17:55:25 +0700

    add easy localization

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,19 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:portfolio/app/portfolio.dart';
+import 'package:portfolio/presentation/resources/language_res.dart';
 
-void main() {
-  runApp(const PortfolioApp());
+void main() async {
+  await EasyLocalization.ensureInitialized();
+
+  runApp(
+    EasyLocalization(
+      supportedLocales: const [bahasaLocal, englishLocal],
+      path: assetsPathLocalization,
+      saveLocale: true,
+      startLocale: bahasaLocal,
+      fallbackLocale: bahasaLocal,
+      child: const PortfolioApp(),
+    ),
+  );
 }
---
+++ lib/presentation/resources/language_res.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const String bahasa = "ID";
+const String english = "EN";
+
+enum LanguageType { bahasa, english }
+
+const String assetsPathLocalization = "assets/translations";
+
+const Locale englishLocal = Locale("en", "US");
+const Locale bahasaLocal = Locale("id", "ID");
