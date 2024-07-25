bf83825a628e891916be68f014a317cfea58e5e3
iyan <andi.ilhamsyah@digi-id.id>
2024-07-25 18:21:46 +0700

    feat: slicing education page

--- lib/presentation/features/main/pages/educations/education_page.dart
+++ lib/presentation/features/main/pages/educations/education_page.dart
@@ -1,4 +1,8 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:portfolio/app/extension.dart';
+import 'package:portfolio/presentation/resources/string_res.dart';
+import 'package:portfolio/presentation/widgets/app_text.dart';
 
 class EducationPage extends StatefulWidget {
   const EducationPage({super.key});
@@ -10,6 +14,24 @@ class EducationPage extends StatefulWidget {
 class _EducationPageState extends State<EducationPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SingleChildScrollView(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                AppText.semiBold(
+                  StringResMain.educationTitle.tr(),
+                ),
+              ],
+            ),
+          ).marginSymmetric(vertical: 34, horizontal: 18),
+        ],
+      ),
+    );
   }
 }
